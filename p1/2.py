546e49f413aa936cf05df5ff52196c1cf3bc5c66
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-03-10 19:55:28 +0330

    Update 2.py

--- p1/2.py
+++ p1/2.py
@@ -3,10 +3,12 @@ import re
 string=str(input())
 #pattern 
 #we can't say [1990-2023] so we decompose this to 4 number 
-pattern=r"[1-2]\d\d\d"
+pattern=r"199\d|20\d\d"
 #answer
 answer=re.findall(pattern, string)
-if(int(answer[0])>=1990):
+
+if(answer !=[]):
+
     print(False)
 else:
     print(True)
